643b8dd351322f3e658defa5a836f3e5cdafee2f
Colbyn Wadman <hello@colbyn.com>
2023-12-22 16:15:04 -0700

    change package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "JsonDataModel",
+    name: "SwiftJsonDataModel",
     platforms: [ .macOS(.v14), .iOS(.v17) ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
